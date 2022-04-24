e529076d38268abd660983a343c26543f943c1f6
Marcelo Mota Ribeiro <marcelomotaribeiro@yahoo.com.br>
2022-04-24 13:51:18 -0300

    Adicionada check com (via favicon) ou sem Cors

--- src/pages/health-panel/health-panel.js
+++ src/pages/health-panel/health-panel.js
@@ -20,24 +20,39 @@ export default class HealthPanel {
                     <div class="card-body">
                         <p id="tag${healthcheck.id}" class="card-title"></p>
                         <p id="moment${healthcheck.id}"></p>
+                        <div id="spinner${healthcheck.id}" class="spinner-border text-primary" role="status">
+                            <span class="visually-hidden">Carregando...</span>
+                        </div>
+                        <img id="img${healthcheck.id}" style="display: none"></img>                     
                     </div>
                 </div>`
             setInterval(() => {
-                var headers = new Headers()
-                headers.append('cache-control', 'no-cache')
-                var init = {
-                    method: 'GET',
-                    headers: headers
+                if (healthcheck.cors) {
+                    const loadImgFavIcon = () => this.update({ status: healthcheck.expectedStatus, healthcheck: healthcheck })
+                    const errorImgFavIcon = () => this.update({ status: -1, healthcheck: healthcheck })
+                    document.getElementById(`img${healthcheck.id}`).onload = loadImgFavIcon
+                    document.getElementById(`img${healthcheck.id}`).onerror = errorImgFavIcon
+                    document.getElementById(`img${healthcheck.id}`).src = healthcheck.url
+                } else {
+                    var headers = new Headers()
+                    headers.append('cache-control', 'no-cache')
+                    var init = {
+                        method: 'GET',
+                        headers: headers,
+                        mode: 'cors'
+                    }
+                    var request = new Request(healthcheck.url)                           
+                    fetch(request, init)
+                    .then(response => this.update({ status: response.status, healthcheck: healthcheck }))
+                    .catch(() => this.update({ status: -1, healthcheck: healthcheck }))
                 }
-                var request = new Request(healthcheck.url)                           
-                fetch(request, init)
-                .then(response => this.update({ status: response.status, healthcheck: healthcheck }))
-                .catch(() => this.update({ status: -1, healthcheck: healthcheck }))
+
             }, 5000);                
         }
     }
 
     update(event) {
+        document.getElementById(`spinner${event.healthcheck.id}`).style = 'display: none;'
         document.getElementById(`image${event.healthcheck.id}`).src = `${event.status == event.healthcheck.expectedStatus ? 'success' : 'fail'}.png`
         document.getElementById(`image${event.healthcheck.id}`).alt = event.status == event.healthcheck.expectedStatus ? 'success' : 'fail'
         document.getElementById(`tag${event.healthcheck.id}`).innerHTML = `<small>${event.healthcheck.tag}</small>`
